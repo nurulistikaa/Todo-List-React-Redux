0fcd0287d11e60c6f18bc10f40a7caaff4afd96d
Nurul Istika <nurulistika92@gmail.com>
2023-10-29 18:54:05 +0700

    Memperbarui import DOM dan store

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './store/store';
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const container = document.getElementById('root');
+
+const root = createRoot(container);
+
+root.render(<Provider store={store}>
+  <App />
+</Provider>);
