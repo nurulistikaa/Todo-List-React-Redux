a6fb75e13034aafaf919df412f22c553afc7efaa
Nurul Istika <nurulistika92@gmail.com>
2023-10-29 18:47:51 +0700

    membuat action types store

---
+++ src/store/actionTypes.js
@@ -0,0 +1,5 @@
+export const ADD_TODO = 'ADD_TODO';
+export const DELETE_TODO = 'DELETE_TODO';
+export const EDIT_TODO = 'EDIT_TODO';
+export const TOGGLE_TODO_STATUS = 'TOGGLE_TODO_STATUS';
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
